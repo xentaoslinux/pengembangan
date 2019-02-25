41b2e22af53ab43d03d3a27aa45c1e3dff28aeef
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-02-25 22:54:33 +0700

    Update

--- i386/MAKEISO.sh
+++ i386/MAKEISO.sh
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-versi="1.3LTS"
+versi="2.0LTS"
 codename=arok
 de=xfce
 version_kernel="4.13.0-19"
@@ -38,9 +38,9 @@ sudo mksquashfs root dvd/casper/filesystem.squashfs -b 1048576 -comp xz -Xdict-s
 cd dvd && sudo rm MD5SUMS
 find -type f -print0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo tee MD5SUMS
 cd ..
-sudo mkisofs -r -V "xentaos-1.3LTS-xfce-i386" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ./xentaos-1.3LTS-xfce-i386-$jahitan.iso dvd
-sudo chmod 777 xentaos-1.3LTS-xfce-i386-$jahitan.iso
-isohybrid xentaos-1.3LTS-xfce-i386-$jahitan.iso
-md5sum xentaos-1.3LTS-xfce-i386-$jahitan.iso   >   xentaos-1.3LTS-xfce-i386-$jahitan.iso.md5msum
-sha1sum xentaos-1.3LTS-xfce-i386-$jahitan.iso  >   xentaos-1.3LTS-xfce-i386-$jahitan.iso.sha1sum
-sha3sum xentaos-1.3LTS-xfce-i386-$jahitan.iso  >   xentaos-1.3LTS-xfce-i386-$jahitan.iso.sha3sum
+sudo mkisofs -r -V "xentaos-2.0LTS-xfce-i386" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ./xentaos-2.0LTS-xfce-i386-$jahitan.iso dvd
+sudo chmod 777 xentaos-2.0LTS-xfce-i386-$jahitan.iso
+isohybrid xentaos-2.0LTS-xfce-i386-$jahitan.iso
+md5sum xentaos-2.0LTS-xfce-i386-$jahitan.iso   >   xentaos-2.0LTS-xfce-i386-$jahitan.iso.md5msum
+sha1sum xentaos-2.0LTS-xfce-i386-$jahitan.iso  >   xentaos-2.0LTS-xfce-i386-$jahitan.iso.sha1sum
+sha3sum xentaos-2.0LTS-xfce-i386-$jahitan.iso  >   xentaos-2.0LTS-xfce-i386-$jahitan.iso.sha3sum
